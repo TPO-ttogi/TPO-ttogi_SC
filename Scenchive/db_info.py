cad982042be874362423a64f909ad185d3c7e911
Choi YeJin <amy350500@gmail.com>
2023-09-04 22:48:23 +0900

    보안 정보 삭제

--- Scenchive/db_info.py
+++ Scenchive/db_info.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 # MySQL 서버와 연결 -> !! 깃헙 push 전 암호화 필수 !!
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="0128",
-  database="scenchive"
+  host="****",
+  user="****",
+  password="****",
+  database="****"
 )
--- Scenchive/s3test.py
+++ Scenchive/s3test.py
@@ -3,10 +3,10 @@ import boto3
 def s3_connection():
     try:
         s3 = boto3.client(
-            service_name = "s3",
-            region_name = "ap-northeast-2",
-            aws_access_key_id = "AKIAWTXFO4EJSQNOR4GW",
-            aws_secret_access_key = "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",
+            service_name = "****",
+            region_name = "****",
+            aws_access_key_id = "****",
+            aws_secret_access_key = "****",
         )
     except Exception as e:
         print(e)
